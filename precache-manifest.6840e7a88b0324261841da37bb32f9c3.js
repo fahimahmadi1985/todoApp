203e8e5a0971d01a361f2b5b6f8f92f04cd03ddd
fahimahmadi <a.fahimahmadi@gmail.com>
2019-11-06 02:53:47 +0100

    Updates

---
+++ precache-manifest.6840e7a88b0324261841da37bb32f9c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f64b30cbec655767125cd7cf458d241",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "6c4a017f7a8b582cb62b",
+    "url": "/todoApp/static/js/2.5909717d.chunk.js"
+  },
+  {
+    "revision": "ec3b73566a092c2ba245",
+    "url": "/todoApp/static/js/main.06f5251b.chunk.js"
+  },
+  {
+    "revision": "26936dad0fe307584f01",
+    "url": "/todoApp/static/js/runtime-main.180a6457.js"
+  }
+]);
